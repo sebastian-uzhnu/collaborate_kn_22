36f8a59f99b9fdd15eb688c4221d9d134fd2b6fc
College <haha@gmail.com>
2025-06-05 13:01:54 +0300

    new coomit 223

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("fdsfsdfsd");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a simple C# console application.");
 
-
+COinsole
 Console.WriteLine("S added.");
 Console.WriteLine("S added.2");
 ddConsole.WriteLine("S added.3");
